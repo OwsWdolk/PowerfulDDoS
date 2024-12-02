4fc28608a5f8d2d33f7cd14319c53c9ed51e33d9
Ows | Wdolk <talhacetin988@gmail.com>
2024-12-02 16:43:53 +0300

    PowerFulDDoS.py

--- PowerFulDDoS.py
+++ PowerFulDDoS.py
@@ -1,14 +1,77 @@
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from scapy.all import IP, TCP, send
-import os, threading, requests
+import os, threading, requests, random
 from selenium import webdriver
-from banner import banner
 from colorama import *
 from os import system
 init(autoreset=True)
 system('cls||clear')
 
+a = Fore.RED+"""
+ █     █░▓█████▄  ▒█████   ██▓     ██ ▄█▀
+▓█░ █ ░█░▒██▀ ██▌▒██▒  ██▒▓██▒     ██▄█▒ 
+▒█░ █ ░█ ░██   █▌▒██░  ██▒▒██░    ▓███▄░ 
+░█░ █ ░█ ░▓█▄   ▌▒██   ██░▒██░    ▓██ █▄ 
+░░██▒██▓ ░▒████▓ ░ ████▓▒░░██████▒▒██▒ █▄
+░ ▓░▒ ▒   ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▒ ▓▒
+  ▒ ░ ░   ░ ▒  ▒   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒ ▒░
+  ░   ░   ░ ░  ░ ░ ░ ░ ▒    ░ ░   ░ ░░ ░ 
+    ░       ░        ░ ░      ░  ░░  ░   
+          ░
+"""
+b = Fore.RED+"""
+░  ░░░░  ░░       ░░░░      ░░░  ░░░░░░░░  ░░░░  ░
+▒  ▒  ▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒  ▒▒
+▓        ▓▓  ▓▓▓▓  ▓▓  ▓▓▓▓  ▓▓  ▓▓▓▓▓▓▓▓     ▓▓▓▓
+█   ██   ██  ████  ██  ████  ██  ████████  ███  ██
+█  ████  ██       ████      ███        ██  ████  █
+"""
+c = Fore.RED+"""
+██╗    ██╗██████╗  ██████╗ ██╗     ██╗  ██╗
+██║    ██║██╔══██╗██╔═══██╗██║     ██║ ██╔╝
+██║ █╗ ██║██║  ██║██║   ██║██║     █████╔╝ 
+██║███╗██║██║  ██║██║   ██║██║     ██╔═██╗ 
+╚███╔███╔╝██████╔╝╚██████╔╝███████╗██║  ██╗
+ ╚══╝╚══╝ ╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝
+"""
+d = Fore.RED+"""
+ .S     S.    .S_sSSs      sSSs_sSSs    S.       .S    S.   
+.SS     SS.  .SS~YS##b    d##SP~YS##b   SS.     .SS    SS.  
+S%S     S%S  S%S   `S#b  d%S'     `S#b  S%S     S%S    S&S  
+S%S     S%S  S%S    S%S  S%S       S%S  S%S     S%S    d*S  
+S%S     S%S  S%S    S&S  S&S       S&S  S&S     S&S   .S*S  
+S&S     S&S  S&S    S&S  S&S       S&S  S&S     S&S_sdSSS   
+S&S     S&S  S&S    S&S  S&S       S&S  S&S     S&S~YSSY#b  
+S&S     S&S  S&S    S&S  S&S       S&S  S&S     S&S    `S%  
+S*S     S*S  S*S    d*S  S*b       d*S  S*b     S*S     S%  
+S*S  .  S*S  S*S   .S*S  S*S.     .S*S  S*S.    S*S     S&  
+S*S_sSs_S*S  S*S_sdSSS    SSSbs_sdSSS    SSSbs  S*S     S&  
+SSS~SSS~S*S  SSS~YSSY      YSSP~YSSY      YSSP  S*S     SS  
+                                                SP          
+                                                Y
+"""
+e = Fore.RED+"""
+/| s\  s\  .s5SSSs\   /SSSs.  .s        .s    s.  
+s| SS\ SS| SS    SS| sS    SS. sS        sS    SS. 
+sS S%S S%S SS    S%S sS    S%S sS        sS    S%S 
+SS S%S S%S SS    S%S SS    S%S SS        SS    S%S 
+SS S%S S%S SS    S%S SS    S%S SS        SSSSs.S:' 
+SS S%S S%S SS    S%S SS    S%S SS        SS  "SS\  
+SS `:; `:; SS    `:; SS    `:; SS        SS    `:; 
+SS ;,. ;,. SS    ;,. SS    ;,. SS    ;,. SS    ;,. 
+`:;;:'`::' ;;;;;;;;| `:;;;;;:' `:;;;;;:' :;    ;:'
+"""
+f = Fore.RED+"""
+    :::       ::: :::::::::   ::::::::  :::        :::    ::: 
+   :+:       :+: :+:    :+: :+:    :+: :+:        :+:   :+:   
+  +:+       +:+ +:+    +:+ +:+    +:+ +:+        +:+  +:+     
+ +#+  +:+  +#+ +#+    +:+ +#+    +:+ +#+        +#++:++       
++#+ +#+#+ +#+ +#+    +#+ +#+    +#+ +#+        +#+  +#+       
+#+#+# #+#+#  #+#    #+# #+#    #+# #+#        #+#   #+#       
+###   ###   #########   ########  ########## ###    ###
+"""
+banner = random.choice(a,b,c,d,e,f)
 print(banner)
 
 guc = input(f"""\n
